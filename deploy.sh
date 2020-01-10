23f7c27a6035f084014efc6635df8fb72c9fc3dc
Gerald Unterrainer <gu_falcon@hotmail.com>
2020-01-10 23:22:39 +0100

    Next try

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 ApiKey=$1
 
 #nuget pack ./MyStromRestApiCSharp/MyStromRestApiCSharp.csproj -Verbosity detailed -Symbols -SymbolPackageFormat snupkg -Prop Configuration=Release
-msbuild -t:pack ./MyStromRestApiCSharp/MyStromRestApiCSharp.csproj -p:NuspecFile=./MyStromRestApiCSharp/MyStromRestApiCSharp.nuspec
+msbuild -t:pack ./MyStromRestApiCSharp/MyStromRestApiCSharp.csproj -p:NuspecFile=./MyStromRestApiCSharp/MyStromRestApiCSharp.nuspec -p:IncludeSymbols=true
 nuget push ./MyStromRestApiCSharp.*.nupkg -Verbosity detailed -ApiKey $ApiKey -source https://www.nuget.org
