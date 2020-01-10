bfff457daf1de35f45d45a8979a162398ceb25f3
Gerald Unterrainer <gu_falcon@hotmail.com>
2020-01-10 12:06:23 +0100

    Switch nuget to release

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 ApiKey=$1
 
-nuget pack ./MyStromRestApiCSharp/MyStromRestApiCSharp.csproj -Verbosity detailed -Symbols -SymbolPackageFormat snupkg
+nuget pack ./MyStromRestApiCSharp/MyStromRestApiCSharp.csproj -Verbosity detailed -Symbols -SymbolPackageFormat snupkg -Prop Configuration=Release
 nuget push ./MyStromRestApiCSharp.*.nupkg -Verbosity detailed -ApiKey $ApiKey -source https://www.nuget.org
