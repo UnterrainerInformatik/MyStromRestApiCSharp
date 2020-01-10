eb95df13b40e06ab0f5b3deaae9c8bcef8e7fb82
Gerald Unterrainer <gu_falcon@hotmail.com>
2020-01-10 11:39:58 +0100

    Correct nuget pack call

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 ApiKey=$1
 
-nuget pack -sym ./MyStromRestApiCSharp/MyStromRestApiCSharp.csproj -Verbosity detailed
+nuget pack -Symbols ./MyStromRestApiCSharp/MyStromRestApiCSharp.csproj -Verbosity detailed
 nuget push ./MyStromRestApiCSharp.*.nupkg -Verbosity detailed -ApiKey $ApiKey -source https://www.nuget.org
